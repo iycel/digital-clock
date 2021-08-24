31210fa694f7448bf3abd63709d34c2953bc4211
iycel <ismailycel@gmail.com>
2021-08-24 20:46:12 +0300

    digital-clock

---
+++ app.js
@@ -0,0 +1,52 @@
+const showTime = () =>  {
+    let time = new Date();
+    let hour = time.getHours();
+    let minute = time.getMinutes();
+    let second = time.getSeconds();
+    if (hour < 10){
+        document.querySelector(".hour").innerHTML = "0" + hour;
+    }else{
+        document.querySelector(".hour").innerHTML = hour;
+    }
+    if (minute < 10) {
+      document.querySelector(".minute").innerHTML = "0" + minute;
+    }else{
+        document.querySelector(".minute").innerHTML = minute;
+    }
+    if (second < 10) {
+      document.querySelector(".second").innerHTML = "0" + second;
+    }else{
+        document.querySelector(".second").innerHTML = second;
+    }
+    if (hour < 7){
+        document.body.style.backgroundImage = "url(./assets/Night.jpg)";
+    }
+    else if(hour>=7 && hour < 11){
+        document.body.style.backgroundImage = "url(./assets/Sunshine.jpg)";
+    }
+    else if(hour>=11 && hour<19){
+        document.body.style.backgroundImage = "url(./assets/Afternoon.jpg)";
+    }
+    else{
+        document.body.style.backgroundImage = "url(./assets/Night.jpg)";
+    }
+}
+
+
+setInterval(() => {
+    showTime();
+}, 1000);
+
+
+
+// let counter = 0;
+
+// function showTime() {
+//     const timeLabel = document.querySelector(".time");
+//     const time = new Date();
+//     timeLabel.innerHTML = time.toLocaleTimeString();
+//     counter ++;
+// }
+// const id = setInterval(showTime, 1000);
+
+
